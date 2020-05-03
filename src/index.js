6450b3623f0c1c5c7297c20ee257dd18b263c8c4
KTran-Tech <kevinmt191919@gmail.com>
2020-05-03 16:32:25 -0500

    Connecting Store to React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//store will most often take a reducer as an input
+const store = createStore(reducer);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
