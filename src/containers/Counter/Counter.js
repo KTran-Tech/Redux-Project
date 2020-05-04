6aaa05265ac220f28501a2b7ef1566846bbb18a5
KTran-Tech <kevinmt191919@gmail.com>
2020-05-03 23:04:48 -0500

    Updating the state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,39 +5,27 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+    
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 10" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Results</button>
+                <ul>
+                    <li></li>
+                </ul>
             </div>
         );
     }
 }
+
+
 //pass in current state and returns newly replicated state
 const mapStateToProps = state => {
     return {
@@ -46,4 +34,19 @@ const mapStateToProps = state => {
 }
 
 
-export default connect(mapStateToProps)(Counter);
+/* To visualize this better, look at it like this 
+mapDisptchToProps({type: 'SUBTRACT', val:10})
+*/
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD',val:10 }),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val:10}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT', }),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT', })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,46 @@
 
 const initialState = {
-
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-    return state;
-}
+
+    switch (action.type) {
+
+        case 'INCREMENT':
+            return{
+/*spreading out all the data from the initial state object*/
+//returning the UPDATED version of the state object and overriding the old one 
+                    ...state,
+                    counter: state.counter + 1 
+            }
+        case 'DECREMENT':
+                return{
+                    ...state,
+                    counter: state.counter - 1 
+                }
+        case 'ADD':
+            return{
+                    ...state,
+                    counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+                return{
+                    ...state,
+                    counter: state.counter - action.val
+                }
+        case 'STORE_RESULTS':
+                return{
+//.concat() is like .push() but it is an immutable way of creating a new array which is what we want
+                    ...state,
+                    results: state.results.concat(state.counter)
+                }       
+        }
+        return state;         
+    }
+
+
+
 
 export default reducer;
