fdec74b6f9375ef5b9de8d3957c87732f3f837b3
KTran-Tech <kevinmt191919@gmail.com>
2020-05-06 23:32:09 -0500

    done

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -9,23 +9,26 @@ const initialState = {
     results: []
 }
 
+
+const deleteResult = (state, action) =>{
+        //'filter' loops through the current array and returns a new array
+        /*If each object has an id NOT equal to the clicked id, then return TRUE, meaning
+        it should keep existing, else if FALSE, then remove it from the newly created array*/
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return updateObject(state, {results: updatedArray})
+
+    }
+
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
 
-        case actionTypes.STORE_RESULT:
-            return updateObject(state, {results: state.results.concat({id: Date.now(), value: action.result})})
+        case actionTypes.STORE_RESULT : return updateObject(state, {results: state.results.concat({id: Date.now(), value: action.result})})
     
-        case actionTypes.DELETE_RESULT:
-            //'filter' loops through the current array and returns a new array
-            /*If each object has an id NOT equal to the clicked id, then return TRUE, meaning
-            it should keep existing, else if FALSE, then remove it from the newly created array*/
-                const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-
-            return updateObject(state, {results: updatedArray})
-
+        case actionTypes.DELETE_RESULT : return deleteResult(state,action)
                 
         }
+
         //returning the NEW state to Counter.js 
         return state;         
     }
